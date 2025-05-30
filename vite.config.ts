9503a596d1fc1d99a740487add99b1bc51b798f2
feng <oscarfli@163.com>
2025-05-30 19:45:19 +0200

    change vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Lifeng-iatom.github.io/', // must match your repo name
+  base: '/', // must match your repo name
   plugins: [react()],
 })
