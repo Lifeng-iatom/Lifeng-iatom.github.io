850ca78575935a532cafbc0634fa803d02e7883c
feng <oscarfli@163.com>
2025-05-30 19:18:41 +0200

    add gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',  // or just remove this line
+  base: '/Lifeng-iatom.github.io/', // must match your repo name
   plugins: [react()],
-});
+})
