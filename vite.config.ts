7f03d60908e082d16f4bbd567a7fb0b0b6a482a9
feng <oscarfli@163.com>
2025-05-30 18:01:57 +0200

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',  // or just remove this line
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
